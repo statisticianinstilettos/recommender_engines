ff07e7e7bb0696da2988240751a797380eef5ce5
Claire <longoclaire@gmail.com>
2019-04-03 14:28:31 -0600

    update imports

--- src/content_filtering/content_filtering.py
+++ src/content_filtering/content_filtering.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import pairwise_distances
 
 
 class ContentRecommender(object):
@@ -11,24 +11,19 @@ class ContentRecommender(object):
         if similarity_metric not in ["cosine", "euclidean", "manhattan", "jaccard"]:
             return ValueError("similarity_metric must be cosine, euclidean, manhattan, or jaccard")
 
-        # get style variant's feature vector
         item_vector = np.array(feature_matrix.loc[seed_item]).reshape(1, -1)
 
-        # calculate similarity
         similarities = self._choose_similarity(item_vector, feature_matrix, similarity_metric)
-        similarities = pd.DataFrame(similarities, index=feature_matrix.index.tolist())
-        similarities.columns = ['similarity_score']
-        similarities.sort_values('similarity_score', ascending=False, inplace=True)
 
-        # return top n similar items with similarity scores
-        similar_items = similarities.head(n).index.values.tolist()
-        scores = similarities.head(n).similarity_score.values.tolist()
+        similar_items, scores = self._get_top_items(similarities, n)
 
         return {"similar_items": similar_items, "score": np.round(scores, 5)}
 
+
     @staticmethod
     def _choose_similarity(item_vector, feature_matrix, similarity_metric):
-
+        '''calculate similarity scores based on specified metric.'''
+        
         if similarity_metric == "cosine":
             similarities = 1 - pairwise_distances(X=feature_matrix, Y=item_vector, metric="cosine")
         elif similarity_metric == "euclidean":
@@ -37,4 +32,19 @@ class ContentRecommender(object):
             similarities = 1 - pairwise_distances(X=feature_matrix, Y=item_vector, metric="manhattan")
         elif similarity_metric == "jaccard":
             similarities = 1 - pairwise_distances(X=feature_matrix, Y=item_vector, metric="hamming")
+
+        similarities = pd.DataFrame(similarities, index=feature_matrix.index.tolist())
+        similarities.columns = ['similarity_score']
+        similarities.sort_values('similarity_score', ascending=False, inplace=True)
+
         return similarities
+
+
+    @staticmethod
+    def _get_top_items(similarities, n):
+        '''return top n similar items with similarity scores'''
+
+        similar_items = similarities.head(n).index.values.tolist()
+        scores = similarities.head(n).similarity_score.values.tolist()
+
+        return similar_items, scores
